9de33447a2b54846a9c85d66cd690bb61923c93b
Íñigo Aréjula Aísa <arejula27@gmail.com>
2024-05-23 14:18:43 +0200

    refac: create middleware file

--- internal/server/middlewares.go
+++ internal/server/middlewares.go
@@ -3,10 +3,35 @@ package server
 import (
 	"net/http"
 
+	"encoding/json"
 	"github.com/labstack/echo/v4"
+	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 )
 
+// AuthorizationMiddleware is a middleware to check if the user is authorized
+func AuthorizationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		//check the session is valid
+		userJSON, err := gothic.GetFromSession("user", c.Request())
+		if err != nil {
+			return c.JSON(401, map[string]string{"message": "Unauthorized"})
+		}
+		var user goth.User
+		err = json.Unmarshal([]byte(userJSON), &user)
+
+		if err != nil {
+			return c.JSON(401, map[string]string{"message": "Unauthorized"})
+		}
+		c.Set("user", User{
+			Email: user.Email,
+			Name:  user.Name,
+		})
+		return next(c)
+	}
+
+}
 func (s *Server) createUserMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		user := c.Get("user").(User)
